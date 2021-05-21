1532327b2d203efdbd685f9a3ccb3d3868853dd9
Paulo Henrique <paulopradella@outlook.com>
2021-05-21 20:50:52 -0300

    Exemplos presentes no material da Semana 1 do curso de GTI da PUCPR

---
+++ POO/Atividades/src/semana_1/Exemplo_9.java
@@ -0,0 +1,14 @@
+package semana_1;
+
+public class Exemplo_9 {
+
+	public static void main(String[] args) {
+		String [] estudantes = new String[] {"Maria", "Pedro", "João"};
+		
+		for(String estudante: estudantes) {
+			System.out.println(estudante);
+		}
+
+	}
+
+}
